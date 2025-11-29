fb5c7b7e7c7f36ef64efa8e8975c63817d1db3de
madesh404 <arulmadesh1457@gmail.com>
2025-11-29 15:00:39 -0500

    nyan_cat.gif

--- create_gif.py
+++ create_gif.py
@@ -2,12 +2,12 @@ import imageio.v3 as iio
 import os
 
 folder1 = "GIF Python"
-folder2 = "png_frames"   # <-- put your folder name here
+folder2 = "Nyan_Cat_Frames"   # <-- put your folder name here
 
-filenames = [os.path.join(folder1, folder2, f"frame_{i:02d}.png") 
-             for i in range(50)]
+filenames = [os.path.join(folder1, folder2, f"nyan-cat{i}.png") 
+             for i in range(1,4)]     # Adjust range as per number of frames
 
 images = [iio.imread(filename) for filename in filenames]
 
-iio.imwrite("obito_mangekyou.gif", images, duration=1, loop=0)
+iio.imwrite("nyan_cat.gif", images, duration=1, loop=0)
 print("GIF created successfully.")
