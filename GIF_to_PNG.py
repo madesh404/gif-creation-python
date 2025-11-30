fb2524dcd57021585f5d3afb5ca84654c3138213
madesh404 <arulmadesh1457@gmail.com>
2025-11-30 14:07:10 -0500

    cat.gif, obito_png_frames (not changed)

--- GIF_to_PNG.py
+++ GIF_to_PNG.py
@@ -2,12 +2,12 @@ from PIL import Image
 import os
 
 folder1 = "GIF Python"
-folder2 = "Obito_GIF_Frames"
-output_folder = "png_frames"
+folder2 = "Cat_GIF_Frames"
+output_folder = "cat_png_frames"
 os.makedirs(output_folder, exist_ok=True)
 
-for i in range(50):
-    gif_path = os.path.join(folder1, folder2, f"frame_{i:02d}_delay-0.1s.gif")
+for i in range(54):
+    gif_path = os.path.join(folder1, folder2, f"frame_{i:02d}_delay-0.06s.gif")
     img = Image.open(gif_path)
 
     # Extract only the first frame
--- create_gif.py
+++ create_gif.py
@@ -2,12 +2,12 @@ import imageio.v3 as iio
 import os
 
 folder1 = "GIF Python"
-folder2 = "Nyan_Cat_Frames"   # <-- put your folder name here
+folder2 = "cat_png_frames"   # <-- put your folder name here
 
-filenames = [os.path.join(folder1, folder2, f"nyan-cat{i}.png") 
-             for i in range(1,4)]     # Adjust range as per number of frames
+filenames = [os.path.join(folder1, folder2, f"frame_{i:02d}.png") 
+             for i in range(54)]     # Adjust range as per number of frames
 
 images = [iio.imread(filename) for filename in filenames]
 
-iio.imwrite("nyan_cat.gif", images, duration=1, loop=0)
+iio.imwrite("cat.gif", images, duration=1, loop=0)
 print("GIF created successfully.")
